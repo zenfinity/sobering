2962b4f4fa4a8c20c63bf7911d89db3de162cb13
Ian Mac Moore <ianmac@blueplusred.com>
2023-08-07 01:11:52 -0500

    New map in index, and added scale legend

--- js/index.js
+++ js/index.js
@@ -1,140 +1,183 @@
 /////////////////////////// World Map ///////////////////////////
-// Initialize map elements
-const svg = d3.select('#mapInteractive');
-const width = svg.attr("width"); // Grab from html tag
-const height = svg.attr("height");
 
-const projection = d3.geoEqualEarth()
-    .scale(175)
-    .translate([width / 2, height / 2]);
+    // Initialize map elements
+    const svg = d3.select('#mapInteractive');
+    const width = svg.attr("width"); // Grab from html tag
+    const height = svg.attr("height");
 
-const pathGenerator = d3.geoPath().projection(projection);
+    const projection = d3.geoEqualEarth()
+      .scale(175)
+      .translate([width / 2, height / 2]);
 
-const graticule = d3.geoGraticule();
+    const pathGenerator = d3.geoPath().projection(projection);
 
-const g = svg.append('g');
+    const graticule = d3.geoGraticule();
 
-var hexData;
+    const g = svg.append('g');
 
-// projection.fitExtent([[0, 0], [width, height]], g);
-// projection.fitSize([width, height], g);
+    var hexData;
 
-//_________________Zoom
-const zoom = d3.zoom()
-    .extent([[0, 0], [width, height]])
-    .scaleExtent([1, 8])
-    .translateExtent([[-100, -100], [width + 90, height + 100]])
-    .on("zoom", zoomed);
+    //_________________ Helper Functions
+    //Make a gradient rectangle of the color scale being used
+    //id must be an svg with width and heigth attributes assigned
+    function drawScale(id, interpolator) {
+        // console.log(id)
+      var data = Array.from(Array(100).keys());
 
-svg.call(zoom)
+      var cScale = d3.scaleSequential()
+        .interpolator(interpolator)
+        .domain([0, 99]);
 
-function zoomed({ transform }) {
-    g.attr("transform", transform);
-}
+      var xScale = d3.scaleLinear()
+        .domain([0, 99])
+        .range([0, 580]);
 
-function reset() {
-    svg.transition()
+      var u = d3.select("#" + id)
+        .selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", (d) => Math.floor(xScale(d)))
+        .attr("y", 0)
+        .attr("height", 40)
+        .attr("width", (d) => {
+          if (d == 99) {
+            return 6;
+          }
+          return Math.floor(xScale(d + 1)) - Math.floor(xScale(d)) + 1;
+        })
+        .attr("fill", (d) => cScale(d));
+    }//drawScale
+
+
+    
+    //_________________ Zoom
+    const zoom = d3.zoom()
+      .extent([[0, 0], [width, height]])
+      .scaleExtent([1, 8])
+      .translateExtent([[-100, -100], [width + 90, height + 100]])
+      .on("zoom", zoomed);
+
+    svg.call(zoom)
+
+    function zoomed({ transform }) {
+      g.attr("transform", transform);
+    }
+
+    function reset() {
+      svg.transition()
         .duration(750)
         .call(zoom.transform, d3.zoomIdentity);
-}
+    }
 
-d3.select("#reset")
-    .on("click", reset)
-
-
+    d3.select("#reset")
+      .on("click", reset)
 
 
-
-// Lat Lng lines
-
-g.insert("path", "path.countries")
-    .datum(graticule.outline)
-    .attr("class", "outline")
-    .attr("d", pathGenerator)
-g.insert("path", "path.countries")
-    .datum(graticule)
-    .attr("class", "graticule")
-    .attr("d", pathGenerator)
+    //_________________ Lat Lng lines
+    g.insert("path", "path.countries")
+      .datum(graticule.outline)
+      .attr("class", "outline")
+      .attr("d", pathGenerator)
+    g.insert("path", "path.countries")
+      .datum(graticule)
+      .attr("class", "graticule")
+      .attr("d", pathGenerator)
 
 
-// Background "Sphere"
-// g.append('path')
-//     .attr('class', 'sphere')
-//     .attr('d', pathGenerator({ type: 'Sphere' }));
+    //_________________ Draw
+    // Map and countries
+    Promise.all([
+      d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv'),
+      d3.csv('/sobering/data/avgDrinksLocations.csv'),
+      d3.json('https://unpkg.com/world-atlas@1.1.4/world/50m.json')
+    ]).then(([tsvData, csvData, topoJSONdata]) => {
+      // hexData = csvData.forEach(function (d) {
+      //     d.litres = +d.litres;
+      //     d["life_expectancy"] = +d["life_expectancy"];
+      // });
+      // console.log(hexData);
+      const countryName = tsvData.reduce((accumulator, d) => {
+        accumulator[d.iso_n3] = {
+          'countryName': d.name,
+          'countryCode': d.iso_a3
+        };
+        return accumulator;
+      }, {});
 
+      const countryCode = tsvData.reduce((accumulator, d) => {
+        accumulator[d.iso_a3] = {
+          'countryID': d.iso_n3,
+          'countryName': d.name //add coordinates here for centroid?
 
+        };
+        return accumulator;
+      }, {});
 
+      // console.log(tsvData)
+      // console.log(countryName)
+      // console.log(countryCode)
+      // console.log(csvData)
 
-
-
-Promise.all([
-    // d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv'),
-    d3.csv('/sobering/data/avgDrinksLocations.csv'),
-    d3.json('https://unpkg.com/world-atlas@1.1.4/world/50m.json')
-]).then(([csvData, topoJSONdata]) => {
-    // hexData = csvData.forEach(function (d) {
-    //     d.litres = +d.litres;
-    //     d["life_expectancy"] = +d["life_expectancy"];
-    // });
-    // console.log(hexData);
-    // const countryName = tsvData.reduce((accumulator, d) => {
-    //     accumulator[d.iso_n3] = d.name;
-    //     return accumulator;
-    // }, {});
-// console.log(csvData)
-
-    const countryName = {};
-    csvData.forEach(d => {
-        countryName[d.country_code] = {
-            'CountryName': d.country_name,
-            'Litres': d.litres = +d.litres, //convert string to number
-            'Life_Expectancy': d.life_expectancy = +d.life_expectancy,
-            'Latitude': d.latitude = +d.latitude,
-            'Longitude': d.longitude = +d.longitude
+      //Create new dict to get lat lng for centroid calc
+      var countryInfo = {};
+      csvData.forEach(d => {
+        countryInfo = {
+          'countryID': countryCode[d.country_code],
+          'countryCode': d.country_code,
+          // 'countryName': d.country_name,
+          'litres': d.litres = +d.litres, //convert string to number
+          'life_Expectancy': d.life_expectancy = +d.life_expectancy,
+          'latitude': d.latitude = +d.latitude,
+          'longitude': d.longitude = +d.longitude
         }
-    });
+      });
 
-    // console.log(countryName)
+      // console.log(countryInfo)
 
-
-
-    const countries = topojson.feature(topoJSONdata, topoJSONdata.objects.countries);
-    g.selectAll('path').data(countries.features)
+      const countries = topojson.feature(topoJSONdata, topoJSONdata.objects.countries);
+      g.selectAll('path').data(countries.features)
         .enter().append('path')
         .attr('class', 'country')
         .attr('d', pathGenerator)
-    // .append('title')
-    // .text(d => countryName[d.id])
+        .append('title')
+        .text(d => countryName[d.id].countryName)
 
-    // g.append("path")
-    //     .datum(graticule)
-    //     .attr("class", "graticule")
-    //     .attr("d", path);
+    });
 
-    // d3.select("svg").insert("path", "path.countries")
-    //     .datum(graticule.outline)
-    //     .attr("class", "graticule outline")
-    //     .attr("d", geoPath)
 
-    g.selectAll(".countryInfo")
-        .data(countryName)
+    // Circle markers separately
+    var myColor = d3.scaleSequential(d3.interpolateBlues)
+      .domain([0, 20]); //manual range for litre min/max
+
+
+    d3.csv('/sobering/data/avgDrinksLocations.csv', function (d) {
+      // console.log(d)
+      d.longitude = +d.longitude;
+      d.latitude = +d.latitude;
+      d.litres = +d.litres;
+      return d;
+    }).then(csvData => {
+      g.selectAll(".marker")
+        .data(csvData)
         .enter().append("circle")
-        .attr('r', 100)
-        // .attr('cx', 10)
-        // .attr('cy', 10)
-        .attr('cx', function(d) {
-            console.log(d)
-            var coords = projection([d.Longitude, d.Latitude])
-            console.log(coords)
-            return 10;
-        })
-        .attr('cy', function(d) {
-            // var coords = projection([d.Longitude, d.Latitude])
-            // console.log(coords)
-            return 10;
-        });
+        .attr("class", "countryInfo")
+        .attr("cx", d => projection([d.longitude, d.latitude])[0])
+        .attr("cy", d => projection([d.longitude, d.latitude])[1])
+        .attr("r", d => Math.sqrt(+d.life_expectancy) * 1) // Adjust the scale as needed
+        .attr("fill", d => myColor(d.litres));
+    });
+
+
+    // Thanks test circle you did well
+    // g
+    //   .append("circle")
+    //   .attr("class", "testCircle")
+    //   .attr('r', 10)
+    //   .attr('cx', 10)
+    //   .attr('cy', 10)
+    // var colorA = "purple", colorB = "orange";
 
 
 
-
-});
+    drawScale("litreScale", d3.interpolateBlues);
